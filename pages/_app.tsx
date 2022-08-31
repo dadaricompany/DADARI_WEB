af966caa3a7ec132e8ea5ea65d1405397a6a81ff
ryuhojin <interarea@naver.com>
2022-08-31 14:09:01 +0900

    font flickering issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import Root from "components/common/Root";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 
+import '../styles/fonts.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
